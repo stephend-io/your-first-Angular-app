993c721266a3c1be83097b3f3708a2014553c638
stephend-io <127882148+stephend-io@users.noreply.github.com>
2024-01-25 10:45:27 +0800

    Lesson 3: Added Housing Location Component

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <div>Housing Location Works!</div>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+
+}
