725f90942e3e4714aa155c166ee02536dfcf826e
stephend-io <127882148+stephend-io@users.noreply.github.com>
2024-01-25 10:52:33 +0800

    Lesson 7: Updated housing location component's template

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -7,7 +7,11 @@ import { HousingLocation } from '../housinglocation';
   standalone: true,
   imports: [],
   template: `
-    <div>Housing Location Works!</div>
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    </section>
   `,
   styleUrl: './housing-location.component.css'
 })
