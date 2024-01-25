8ef0978c838874ba50d35641a24900ec51053548
stephend-io <127882148+stephend-io@users.noreply.github.com>
2024-01-25 10:47:42 +0800

    Lesson 4: Added housinglocation interface and updated HomeComponent's class

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -19,6 +20,19 @@ import { CommonModule } from '@angular/common';
   `,
    styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
+
